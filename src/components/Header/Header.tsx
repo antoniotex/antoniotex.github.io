f0d91174be6bd3c5b1dadbd80313b2fc44a0138e
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-02 09:46:24 -0300

    feat: add navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
 import * as S from './styles.css';
 
 const Header = () => {
-  return <S.Container>Header works!</S.Container>;
+  return (
+    <S.Container>
+      <S.NavBar>
+        <S.Menu>
+          <S.MenuItem>
+            <S.Number>01.</S.Number>
+            <S.Link>{'<sobre mim>'}</S.Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.Number>02.</S.Number>
+            <S.Link>{'<habilidades>'}</S.Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.Number>03.</S.Number>
+            <S.Link>{'<projetos>'}</S.Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.Number>04.</S.Number>
+            <S.Link>{'<experiência>'}</S.Link>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.Number>05.</S.Number>
+            <S.Link>{'<contato>'}</S.Link>
+          </S.MenuItem>
+        </S.Menu>
+      </S.NavBar>
+    </S.Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.css.ts
+++ src/components/Header/styles.css.ts
@@ -1,3 +1,53 @@
 import styled from 'styled-components';
+import { theme } from '../../common/styles/theme.css';
 
-export const Container = styled.nav``;
+export const Container = styled.header`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const NavBar = styled.nav`
+  width: fit-content;
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  position: relative;
+`;
+
+export const MenuItem = styled.li`
+  padding: 16px 0;
+  cursor: pointer;
+  position: relative;
+
+  :after {
+    position: absolute;
+    left: 0px;
+    content: '';
+    height: 40px;
+    width: 0%;
+    border-bottom: solid 3px ${theme.colors.primary};
+    transition: all 0.3s cubic-bezier(0.22, 0.61, 0.36, 1);
+    opacity: 0;
+  }
+
+  :hover:after {
+    opacity: 1;
+    width: 50%;
+  }
+`;
+
+export const Number = styled.span`
+  color: ${theme.colors.primary};
+  font-size: ${theme.font.sizes.medium};
+  margin-right: 8px;
+  font-family: ${theme.font.family.sourceCode};
+  font-weight: ${theme.font.weight.normal};
+  letter-spacing: 0.2px;
+`;
+
+export const Link = styled(Number)`
+  color: ${theme.colors.black};
+  margin-right: 16px;
+`;
