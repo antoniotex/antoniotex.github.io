6830f8a2385092971d9a06a2ccf67a273b93aad2
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-02 10:31:52 -0300

    feat: add SocialNetwork component

--- src/components/SocialNetwork/SocialNetwork.tsx
+++ src/components/SocialNetwork/SocialNetwork.tsx
@@ -1,7 +1,19 @@
 import * as S from './styles.css';
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { TbBrandTelegram } from 'react-icons/tb';
+import { FiGithub } from 'react-icons/fi';
+import { theme } from '../../common/styles/theme.css';
 
 const SocialNetwork = () => {
-  return <S.Container>SocialNetwork works!</S.Container>;
+  return (
+    <S.Container>
+      <FiGithub size={35} color={theme.colors.primary} />
+      <FaLinkedinIn size={35} color={theme.colors.primary} />
+      <AiOutlineWhatsApp size={35} color={theme.colors.primary} />
+      <TbBrandTelegram size={35} color={theme.colors.primary} />
+    </S.Container>
+  );
 };
 
 export default SocialNetwork;
--- src/components/SocialNetwork/styles.css.ts
+++ src/components/SocialNetwork/styles.css.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
+import { theme } from '../../common/styles/theme.css';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: fit-content;
+  height: 300px;
+  position: relative;
+
+  ::after {
+    position: absolute;
+    content: '';
+    background: ${theme.colors.primary};
+    width: 3px;
+    height: 150px;
+    bottom: -200px;
+  }
+`;
