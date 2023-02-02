8b32dd29160e888499a6adef75b300f584104886
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-02 08:27:39 -0300

    style: add style constants

---
+++ src/common/styles/theme.css.ts
@@ -0,0 +1,27 @@
+export const theme = {
+  colors: {
+    white: '#fcfcfc',
+    black: '#383838',
+    gray: '##a7a7a7',
+    lightGray: '#ededed',
+    primary: '#646bff',
+  },
+  font: {
+    weight: {
+      light: 300,
+      normal: 400,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 900,
+    },
+    sizes: {
+      xsmall: '1rem',
+      small: '1.2rem',
+      medium: '1.6rem',
+      large: '2.4rem',
+      xlarge: '3.2rem',
+      xxlarge: '4.0rem',
+    },
+    family: {},
+  },
+};
