28a597cef3cabe6407b3ca7f59ad6314afcae51d
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-03 10:38:20 -0300

    feat(project): add ProjectCard components

---
+++ src/pages/Projects/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,39 @@
+import { Project } from '../../types';
+import * as S from './styles.css';
+import { FaRegFolder } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
+import { CgWebsite } from 'react-icons/cg';
+import { theme } from '../../../../common/styles/theme.css';
+
+type Props = {
+  project: Project;
+};
+
+const ProjectCard: React.FC<Props> = ({ project }) => {
+  return (
+    <S.Container>
+      <S.Top>
+        <FaRegFolder size={35} color={theme.colors.primary} />
+        <FiGithub
+          size={30}
+          color={theme.colors.black}
+          style={{ marginLeft: 20 }}
+        />
+        <CgWebsite
+          size={30}
+          color={theme.colors.black}
+          style={{ marginLeft: 20 }}
+        />
+      </S.Top>
+      <S.Title>{project.title}</S.Title>
+      <S.Description>{project.description}</S.Description>
+      <S.TechContainer>
+        {project.techs.map((tech: string, index: number) => (
+          <S.TechPill>{tech}</S.TechPill>
+        ))}
+      </S.TechContainer>
+    </S.Container>
+  );
+};
+
+export default ProjectCard;
---
+++ src/pages/Projects/components/ProjectCard/styles.css.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { theme } from '../../../../common/styles/theme.css';
+
+export const Container = styled.div`
+  width: 430px;
+  padding: 20px;
+  border-radius: 16px;
+  box-shadow: 0 20px 50px ${theme.colors.black}55;
+  margin-bottom: 50px;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  position: relative;
+
+  > :first-child {
+    position: absolute;
+    left: 0;
+    top: -5px;
+  }
+`;
+
+export const Title = styled.h4`
+  margin: 10px 0;
+  font-size: ${theme.font.sizes.medium};
+`;
+
+export const Description = styled.p`
+  font-size: ${theme.font.sizes.small};
+  margin-bottom: 20px;
+`;
+
+export const TechContainer = styled.div`
+  display: flex;
+`;
+
+export const TechPill = styled.div`
+  background: ${theme.colors.lightGray};
+  font-weight: ${theme.font.weight.normal};
+  margin-right: 8px;
+  padding: 4px 16px;
+  border-radius: 16px;
+`;
