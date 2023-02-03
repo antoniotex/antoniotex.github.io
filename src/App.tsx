97db1f39c1d944594b97672db2008e458625ce30
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-03 09:19:26 -0300

    feat: use news components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import GlobalStyles from './common/styles/global.css';
 import { Header } from './components/Header';
 import Home from './pages/Home';
+import { AboutMe } from './pages/AboutMe';
+import { Skills } from './pages/Skills';
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <AboutMe />
+      <Skills />
       <GlobalStyles />
     </>
   );
