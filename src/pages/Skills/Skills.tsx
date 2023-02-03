340e6640ec8dc6b3ef173c2d480c8621f840370e
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-03 09:18:38 -0300

    feat: add Skills component

---
+++ src/pages/Skills/Skills.tsx
@@ -0,0 +1,52 @@
+import { SectionTag } from '../../components/SectionTag';
+import * as S from './styles.css';
+
+type Skills = {
+  tech: string;
+  level: number;
+};
+
+const Skills: React.FC = () => {
+  const skills: Skills[] = [
+    { tech: 'HTML / CSS', level: 5 },
+    { tech: 'Javascript', level: 5 },
+    { tech: 'Typescript', level: 5 },
+    { tech: 'ReactJs', level: 5 },
+    { tech: 'React Native', level: 4 },
+    { tech: 'Angular 2+', level: 3 },
+    { tech: 'Jest', level: 4 },
+    { tech: 'Styled Components', level: 5 },
+    { tech: 'NodeJs', level: 4 },
+  ];
+
+  return (
+    <SectionTag num='02' title='tecnologias'>
+      <S.Container>
+        {skills.map((skill: Skills, index: number) => (
+          <S.Row>
+            <S.TechText>{skill.tech}</S.TechText>
+            <S.Level key={index}>
+              <S.LevelItem
+                opacity={skill.level >= 1 ? '44' : '00'}
+              ></S.LevelItem>
+              <S.LevelItem
+                opacity={skill.level >= 2 ? '77' : '00'}
+              ></S.LevelItem>
+              <S.LevelItem
+                opacity={skill.level >= 3 ? 'aa' : '00'}
+              ></S.LevelItem>
+              <S.LevelItem
+                opacity={skill.level >= 4 ? 'dd' : '00'}
+              ></S.LevelItem>
+              <S.LevelItem
+                opacity={skill.level >= 5 ? 'ff' : '00'}
+              ></S.LevelItem>
+            </S.Level>
+          </S.Row>
+        ))}
+      </S.Container>
+    </SectionTag>
+  );
+};
+
+export default Skills;
---
+++ src/pages/Skills/styles.css.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { theme } from '../../common/styles/theme.css';
+
+type LevelItemProps = {
+  opacity: string;
+};
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  box-shadow: 0 20px 50px ${theme.colors.black}55;
+  border-radius: 16px;
+`;
+
+export const TechText = styled.h3`
+  font-weight: ${theme.font.weight.normal};
+  font-size: ${theme.font.sizes.medium};
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+
+  :last-child {
+    margin-bottom: 0px;
+  }
+`;
+
+export const Level = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+export const LevelItem = styled.div<LevelItemProps>`
+  position: relative;
+  width: 90px;
+  height: 40px;
+  border-radius: 8px;
+  margin-left: 16px;
+  background: ${(p: LevelItemProps) =>
+    p.opacity === '00' ? theme.colors.lightGray : 'none'};
+
+  ::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: ${theme.colors.primary}${(p: LevelItemProps) => p.opacity};
+    border-radius: 8px;
+  }
+
+  :first-child {
+    margin-left: 0px;
+  }
+`;
