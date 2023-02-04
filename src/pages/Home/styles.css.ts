8108969c129ffd3949ec9fca7dd872dd97d82eef
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-04 11:26:58 -0300

    feat: update Home style

--- src/pages/Home/styles.css.ts
+++ src/pages/Home/styles.css.ts
@@ -7,7 +7,11 @@ export const Container = styled.article`
   margin-top: 200px;
 
   @media (max-width: 1024px) {
-    padding: 0 5px;
+    padding: 0 10px;
     margin-top: 60px;
+  }
+
+  @media (max-width: 425px) {
+    padding: 0 5px;
   }
 `;
