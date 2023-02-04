2629fdc81549f7e2ad5b08a254ccf03cbe60ec2e
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-04 11:25:16 -0300

    feat: update SectionTag component style

--- src/components/SectionTag/styles.css.ts
+++ src/components/SectionTag/styles.css.ts
@@ -7,18 +7,38 @@ export const Container = styled.article`
   flex-wrap: nowrap;
   max-width: 900px;
   margin: 100px auto;
+
+  @media (max-width: 1024px) {
+    padding: 0 20px;
+  }
+
+  @media (max-width: 768px) {
+    margin: 80px auto;
+  }
+
+  @media (max-width: 425px) {
+    padding: 0 20px;
+  }
 `;
 
 export const Open = styled.div`
   display: flex;
   flex-wrap: nowrap;
   margin-bottom: 40px;
+
+  @media (max-width: 768px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const OpenText = styled.h2`
   font-family: ${theme.font.family.sourceCode};
   font-size: ${theme.font.sizes.large};
   font-weight: ${theme.font.weight.extraBold};
+
+  @media (max-width: 425px) {
+    font-size: ${theme.font.sizes.medium};
+  }
 `;
 
 export const Number = styled(OpenText)`
@@ -31,4 +51,7 @@ export const Close = styled(OpenText)`
   color: ${theme.colors.gray};
   align-self: flex-end;
   margin-top: 40px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
 `;
