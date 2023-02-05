95987242b9b51de90f801a995c694355c14fd8d6
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-05 09:32:32 -0300

    feat: update career page with news informations

--- src/pages/Career/Career.tsx
+++ src/pages/Career/Career.tsx
@@ -10,16 +10,20 @@ const Career: React.FC = () => {
           <S.Company>{` @ Foursys. fev/2022 - fev/2023`}</S.Company>
         </S.CareerInfo>
         <S.CareerInfo>
-          <S.Role>{`Desenvolvedor Frontend React`}</S.Role>
-          <S.Company>{` @ Foursys. fev/2022 - fev/2023`}</S.Company>
+          <S.Role>{`Desenvolvedor Frontend React / React Native`}</S.Role>
+          <S.Company>{` @ Dclick. set/2021 - fev/2022`}</S.Company>
         </S.CareerInfo>
         <S.CareerInfo>
-          <S.Role>{`Desenvolvedor Frontend React`}</S.Role>
-          <S.Company>{` @ Foursys. fev/2022 - fev/2023`}</S.Company>
+          <S.Role>{`Analista de Sistemas`}</S.Role>
+          <S.Company>{` @ ACT Digital. jan/2021 - set/2021`}</S.Company>
         </S.CareerInfo>
         <S.CareerInfo>
-          <S.Role>{`Desenvolvedor Frontend React`}</S.Role>
-          <S.Company>{` @ Foursys. fev/2022 - fev/2023`}</S.Company>
+          <S.Role>{`Desenvolvedor Fullstack`}</S.Role>
+          <S.Company>{` @ S2 Holding. ago/2020 - nov/2020`}</S.Company>
+        </S.CareerInfo>
+        <S.CareerInfo>
+          <S.Role>{`Desenvolvedor .NET Júnior`}</S.Role>
+          <S.Company>{` @ Função Sistemas. ago/2019 - ago/2020`}</S.Company>
         </S.CareerInfo>
       </S.Container>
     </SectionTag>
