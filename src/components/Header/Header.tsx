aea607cf958604404c6ae64445116e96d1cb2102
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-09 09:28:21 -0300

    feat: header translations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,16 @@ import { theme } from '../../common/styles/theme.css';
 import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../store/AppContext';
 import { useTheme } from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
   const [menuTop, setMenuTop] = useState(-500);
   const [scrollPosition, setScrollPosition] = useState(0);
+
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
 
   const { colors } = useTheme();
 
@@ -41,23 +47,23 @@ const Header = () => {
           <S.Menu>
             <S.MenuItem href='#sobre'>
               <S.Number>01.</S.Number>
-              <S.Link>{'<sobre mim>'}</S.Link>
+              <S.Link>{t('menu.about')}</S.Link>
             </S.MenuItem>
             <S.MenuItem href='#tecnologias'>
               <S.Number>02.</S.Number>
-              <S.Link>{'<tecnologias>'}</S.Link>
+              <S.Link>{t('menu.skills')}</S.Link>
             </S.MenuItem>
             <S.MenuItem href='#projetos'>
               <S.Number>03.</S.Number>
-              <S.Link>{'<projetos>'}</S.Link>
+              <S.Link>{t('menu.projects')}</S.Link>
             </S.MenuItem>
             <S.MenuItem href='#experiencia'>
               <S.Number>04.</S.Number>
-              <S.Link>{'<experiencia>'}</S.Link>
+              <S.Link>{t('menu.career')}</S.Link>
             </S.MenuItem>
             <S.MenuItem href='#contato'>
               <S.Number>05.</S.Number>
-              <S.Link>{'<contato>'}</S.Link>
+              <S.Link>{t('menu.contact')}</S.Link>
             </S.MenuItem>
           </S.Menu>
         </S.NavBar>
