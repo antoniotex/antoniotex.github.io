93b7358cd66030375387fd0a4ca62921d7eed021
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-09 11:48:20 -0300

    feat: about me translations

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -8,19 +8,7 @@ const AboutMe: React.FC = () => {
   return (
     <S.Container>
       <SectionTag num='01' title={t('section.about')} idItem='sobre'>
-        <S.AboutText>
-          Desenvolvedor de sistemas com mais de 5 anos de experiência atuando em
-          projetos de grande porte. Grande vivência com desenvolvimento
-          responsivo, desenvolvimento mobile, criação e integração de API`s,
-          testes automatizados (unitários, de integração, e2e). <br />
-          <br /> Conhecimentos em: <br />
-          <br />
-          - Linguagens de Programação: Javascript, TypeScript, C# <br />
-          - Frontend: HTML, CSS, SCSS, React, React Native, VueJS, Angular 2+,
-          Redux, Styled Components, Storybook <br />- Backend: NodeJS, .NET
-          Core, SQL Server, MongoDB, Oracle, TypeORM, EF <br />- Testes: Jest,
-          React Testing Library, Cypress
-        </S.AboutText>
+        <S.AboutText>{t('about.description')}</S.AboutText>
       </SectionTag>
     </S.Container>
   );
