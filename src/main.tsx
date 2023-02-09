38e319e6d8edeaf8605db398f9b9bcc10168dc67
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-09 09:28:08 -0300

    feat: i18n configs

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppContextProvider, { AppContext } from './store/AppContext';
 
+import './lib/i18n';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AppContextProvider>
     <App />
